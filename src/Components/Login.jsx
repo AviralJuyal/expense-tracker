49e0f6f35fecc67e14dbe42b04ce6f8a9eb459b7
RupinVijan <rupinvijan@gmail.com>
2022-08-28 16:05:45 +0530

    login signup added

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React , {useState} from "react";
 
 const Login = () => {
+  const [formData, setFormData] = useState({email:"" , password:""});
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    // console.log(formData)
+    const res = await fetch('http://localhost:8080/api/login' , {
+      method:'POST',
+      headers:{
+        "Content-Type": "application/json",
+      },
+      body:JSON.stringify(formData)
+    })
+    const result  = await res.json();
+    console.log(result , 'result')
+    setFormData({
+      email:"" , password:""
+     })
+  }
   return (
     <>
     <div class="log-form">
   <h2>Login to your account</h2>
-  <form>
-    <input type="email" title="username" placeholder="Enter your email address" />
-    <input type="password" title="username" placeholder="password" />
+  <form onSubmit={handleSubmit}>
+  <input type="email" title="username" value={formData.email} onChange = {(e)=>{setFormData({...formData, email:e.currentTarget.value})}} placeholder="Enter your email address" />
+    <input type="password" title="username" value={formData.password} onChange = {(e)=>{setFormData({...formData, password:e.currentTarget.value})}} placeholder="password" />
     <button type="submit" class="btn">Login</button>
     {/* <a class="forgot" href="#">Forgot Username?</a> */}
   </form>
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React , {useState} from 'react'
 
 const Register = () => {
+  const [formData, setFormData] = useState({email:"" , password:""});
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    const res = await fetch('http://localhost:8080/api/signup' , {
+      method:'POST',
+      headers:{
+        "Content-Type": "application/json",
+      },
+      body:JSON.stringify(formData)
+    })
+    const result  = await res.json();
+    console.log(result , 'result')
+   setFormData({
+    email:"" , password:""
+   })
+  }
   return (
     <>
      <div class="log-form">
   <h2>Create New Account </h2>
-  <form>
-    <input type="email" title="username" placeholder="Enter your email address" />
-    <input type="password" title="username" placeholder="password" />
+  <form onSubmit={handleSubmit}>
+    <input type="email" title="username" value={formData.email} onChange = {(e)=>{setFormData({...formData, email:e.currentTarget.value})}} placeholder="Enter your email address" />
+    <input type="password" title="username" value={formData.password} onChange = {(e)=>{setFormData({...formData, password:e.currentTarget.value})}} placeholder="password" />
     <button type="submit" class="btn">Register</button>
   </form>
 </div>
